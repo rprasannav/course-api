a010694c30f3a92fd334b895e3c8b7cbd9c4de92
rpv <rpv@rpvs-MacBook-Pro.local>
2018-10-07 16:21:05 +0530

    Initial commit.

---
+++ src/main/java/io/myjava/springbootstarter/topics/TopicService.java
@@ -0,0 +1,48 @@
+package io.myjava.springbootstarter.topics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	private List<Topic> topics = new ArrayList<>(
+			Arrays.asList(new Topic("springBoot", "Spring Boot Application", "Spring Boot Application Tutorial"),
+					new Topic("java", "Java Application", "Java Tutorial")));
+
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+//		for (Topic t : topics) {
+//			if (t.getId().equals(id)) {
+//				topics.set(topics.indexOf(t), t);
+//			}
+//		}
+
+		for (int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if (t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+
+}
